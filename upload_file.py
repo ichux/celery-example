abbe3fe172ffa11c5f6e691ed819a551b0497a78
Chukwudi Nwachukwu <theichux@gmail.com>
2017-09-25 15:01:20 +0100

    added dynamic content type detector

--- upload_file.py
+++ upload_file.py
@@ -1,27 +1,29 @@
+import mimetypes
+
 import requests
+
+
+def get_content_type(filename):
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
 
 
 def load_file(file_location="/Users/ichux/PycharmProjects/gcore/Mar-2017.csv"):
     file_bytes = open(file_location, 'rb').read()
 
-    url = "http://localhost:8889/upload"
-    payload = "------DataBoundary\r\n"
-    payload += "Content-Disposition: form-data; name=\"file\"; filename=\"Mar-2017.csv\"\r\n"
-    payload += "Content-Type: text/csv\r\n\r\n"
-    payload += '%s\r\n' % file_bytes
-    payload += "------DataBoundary--"
+    payload = '------DataBoundary\r\n'
+    payload += 'Content-Disposition: form-data; name="file"; filename="Mar-2017.csv"\r\n'
+    payload += f'Content-Type: {get_content_type(filename=file_location)}\r\n\r\n'
+    payload += f'{file_bytes}\r\n'
+    payload += '------DataBoundary--'
 
     headers = {
         'content-type': "multipart/form-data; boundary=----DataBoundary",
         'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:46.0) Gecko/20100101 Firefox/46.0",
-        'accept': "*/*",
-        'accept-language': "en-GB,en;q=0.5",
-        'accept-encoding': "gzip, deflate",
-        'connection': "keep-alive"
+        'accept': "*/*", 'accept-language': "en-GB,en;q=0.5",
+        'accept-encoding': "gzip, deflate", 'connection': "keep-alive"
     }
 
-    # print(repr(payload))
-    response = requests.post(url, data=payload, headers=headers)
+    response = requests.post("http://localhost:8889/upload", data=payload, headers=headers)
     return response.json().get("task_id")
 
 
@@ -31,5 +33,5 @@ def ask_status(task_id):
 
 
 if __name__ == '__main__':
-    # print(load_file())
-    print(ask_status("10b5c2e7-f874-4928-8768-8cd7c41c2951"))
+    print(load_file())
+    print(ask_status("02d764ad-c465-4220-b7eb-218995b3ce07"))
