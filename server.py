ec817f9f6cb77dd2f03bbbe1b265ac7809533837
Chukwudi Nwachukwu <theichux@gmail.com>
2017-09-25 13:07:02 +0100

    Cleaned up the code to work with Python 3
    Redis is now used for both the broker_url and the result_backend.
    compute_properties is now cleaned up a bit and a proper upload
    folder was also created.

--- server.py
+++ server.py
@@ -1,79 +1,79 @@
 import os
-from flask import Flask, url_for, jsonify, request, make_response
-from celery import Celery, states
-from flask_cors import CORS, cross_origin
 
 import numpy as np
 import pandas as pd
-
+from celery import Celery, states
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['CELERY_BROKER_URL'] = 'amqp://admin:password@localhost/test'
-app.config['CELERY_RESULT_BACKEND'] = 'amqp://admin:password@localhost/test'
+app.config['CELERY_BROKER_URL'] = 'redis://:4376046362e3d3c9d407c2425809b6da@192.168.56.20:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://:4376046362e3d3c9d407c2425809b6da@192.168.56.20:6379/0'
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
+
+BASE_DIRECTORY = os.path.realpath(os.path.dirname(os.path.realpath(__file__)))
+UPLOADS = os.path.join(BASE_DIRECTORY, 'uploads')
+
+# create a directory for uploads
+if not os.path.exists(UPLOADS):
+    try:
+        os.mkdir(UPLOADS)
+    except (Exception,):
+        pass
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
     file_obj = request.files.get('file')
     file_name = file_obj.filename
 
-    path = os.path.join('./uploads', file_name)
-
+    path = os.path.join(UPLOADS, file_name)
     file_obj.read(0)
 
     try:
         file_obj.save(path)
     except IOError:
-        print 'I/O Error'
+        print('I/O Error')
 
     file_obj.close()
-
     file_task = read_csv_task.apply_async(args=[path])
-
     return make_response(jsonify({'task_id': file_task.task_id}))
+
 
 @app.route('/task/<task_id>', methods=['GET'])
 def check_task_status(task_id):
-
     task = read_csv_task.AsyncResult(task_id)
     state = task.state
-    response = {}
-    response['state'] = state
+
+    response = {'state': state}
 
     if state == states.SUCCESS:
         response['result'] = task.get()
     elif state == states.FAILURE:
         try:
             response['error'] = task.info.get('error')
-        except Exception as e:
-            response['error'] = 'Unknown error occurred'
-        
+        except (Exception,) as e:
+            response['error'] = str(e)
+
     return make_response(jsonify(response))
 
 
 @celery.task(bind=True)
 def read_csv_task(self, path):
-
     self.update_state(state=states.PENDING)
 
     df = pd.read_csv(path)
     result = compute_properties(df)
-    
     return result
 
+
 def compute_properties(df):
-    properties = {}
+    return {'num_rows': len(df), 'num_columns': len(df.columns), 'column_data': get_column_data(df)}
 
-    properties['num_rows'] = len(df)
-    properties['num_columns'] = len(df.columns)
-
-    properties['column_data'] = get_column_data(df)
-
-    return properties
 
 def get_column_data(df):
     result = []
@@ -93,16 +93,15 @@ def get_column_data(df):
             info['max'] = col.max()
         else:
             unique_values = col.unique().tolist()
-            print len(unique_values), len(df)
+            print(len(unique_values), len(df))
             if len(unique_values) < len(df):
                 info['unique_values'] = unique_values
             else:
                 info['unique_values'] = True
 
         result.append(info)
-
     return result
+
 
 if __name__ == '__main__':
     app.run(port=8889, debug=True)
-
